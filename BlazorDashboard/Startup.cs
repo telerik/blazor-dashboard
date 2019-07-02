9a5c8c80ee357dabde635490adc8574d0302d91c
Marin Bratanov <m.bratanov@gmail.com>
2019-07-02 13:08:55 +0300

    remove template leftovers

--- BlazorDashboard/Startup.cs
+++ BlazorDashboard/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorDashboard.Data;
 using BlazorDashboard.DataRetrieval;
 
 namespace BlazorDashboard
